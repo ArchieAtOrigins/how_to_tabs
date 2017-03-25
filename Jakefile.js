6561d33ea39d2731dd7e4919980bbe2b24b33288
ArchieAtOrigins <archie@originscreative.com>
2017-03-25 11:35:20 -0700

    Updated Jake build with node version check

--- Jakefile.js
+++ Jakefile.js
@@ -2,8 +2,18 @@
 (function() {
 	"use strict";
 
+	var EXPECTED_NODE_VERSION = "v0.12.4"
 	desc("Default Build");
-	task("default", function(){
+	task("default", [ "version" ], function(){
 		console.log("\n BUILD SUCCESS\n");
-	})
+	});
+
+	desc("Check Node Version");
+	task("version", function() {
+		console.log("Checking Node Version: .");
+		var actualVersion = process.version;
+		if (actualVersion !== EXPECTED_NODE_VERSION) 
+			fail("Node version does not match: expected " + EXPECTED_NODE_VERSION + ", but was " + actualVersion);
+
+	});
 }());
